5ad03995617c6e0f99d37f0ce3ddc0ac058f5e3d
Alexander Fomin <rezviybelorus@gmail.com>
2023-03-05 12:43:44 +0100

    initial commit

---
+++ src/com/pingwit/part_1/OperatorPriorityExample.java
@@ -0,0 +1,9 @@
+package com.pingwit.part_1;
+
+public class OperatorPriorityExample {
+    public static void main(String[] args) {
+        int expression = 8 / 2 * (2 + 2);
+        System.out.println(expression);
+    }
+
+}
